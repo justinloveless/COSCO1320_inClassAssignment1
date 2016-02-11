5ff961b5091a9aea29e55778fa44f75fae619b90
Justin Loveless <justinlovelessx@gmail.com>
2016-02-11 13:39:59 -0600

    initial commit

---
+++ inClassAssignment1/main.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+#include "Carousel.h"
+#include "Train.h"
+#include "Viking.h"
+#include "RollerCoaster.h"
+
+using namespace std;
+
+int main()
+{
+	
+
+
+	system("pause");
+	return 0;
+}

951a52c538fbade8381715272a31b638adf35ce5
Justin Loveless <justinlovelessx@gmail.com>
2016-02-11 13:50:36 -0600

    added childrenRides

---
+++ inClassAssignment1/ChildrenRide.h
@@ -0,0 +1,20 @@
+#ifndef CHILDRENRIDE_H
+#define CHILDRENRIDE_H
+#include <iostream>
+using namespace std;
+
+class ChildrenRide {
+private :
+	static int numOfChildrenRides;
+	int maxAge;
+public :
+	ChildrenRide();
+	ChildrenRide(string, int);
+	static int getNumOfChildrenRides();
+	int getMaxAge() const; //look up what this means again
+	virtual void restrictions() const;
+	virtual void showInfor() const = 0;
+
+};
+
+#endif

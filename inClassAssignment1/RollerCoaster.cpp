66a8e42d3792492e9956284dad545cb70fb907af
Ayesha96 <ayeshasyeda72@yahoo.com>
2016-02-11 20:51:02 -0600

    rollercoaster.cpp

---
+++ inClassAssignment1/RollerCoaster.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include "RollerCoaster.h"
+using namespace std;
+
+RollerCoaster::RollerCoaster()
+{
+
+}
+RollerCoaster::RollerCoaster(string, int)
+{
+
+}
+void RollerCoaster::showInfo() const;
+{
+
+}
